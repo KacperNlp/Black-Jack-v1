a5bb6d515e3fd81794c1cd260274a119d92fdbcd
Kacper Nalepa <kacper.nlp@wp.pl>
2021-01-15 19:53:32 +0100

    remove methods about score from Player to Deck

--- app/scripts/Deck.js
+++ app/scripts/Deck.js
@@ -34,4 +34,34 @@ export class Deck{
     pickOne(){
         return this.cards.pop();
     }
+
+
+
+    cardsWithTheSameWeight(weight){
+        return this.cards.filter( card => card.weight === weight).length
+    }
+
+    getHandStrength(){
+        if(this.cardsWithTheSameWeight('A').length === 2){
+            return 21;
+        }
+
+        const cardsStrength = this.cards.map((card, id, array) =>{
+
+            if(['K', 'Q', 'J'].includes(card.weight)){
+                return 10;
+            }else if(card.weight === 'A' && array.length === 2){
+                return 11;
+            }else if(card.weight === 'A' && array.length > 2){
+                return 1;
+            }else{
+                return parseInt(card.weight);
+            }
+
+        })
+
+        return cardsStrength.reduce((sum, currentWieght)=>{
+            return sum + currentWieght;
+        });
+    }
 }
