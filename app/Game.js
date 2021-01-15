82c407795cf425f9b0272b0a4c29e5ffa09a7221
Kacper Nalepa <kacper.nlp@wp.pl>
2021-01-15 18:21:56 +0100

    Created class Table where is posibility to add player and dealer cards to table

--- app/Game.js
+++ app/Game.js
@@ -1,18 +1,18 @@
 import  {Deck} from './scripts/Deck.js'
 import { Player } from './scripts/Player.js';
 import {bindToHtml} from './scripts/BindToHtml.js'
+import {Table} from './scripts/Table.js'
 
 const playerCardsContainerId = 'player-cards';
 const dealerCardsContainerId = 'AI-cards';
 
 class Game{
-    constructor({player, playerCards, dealerCards}){
+    constructor({player, tabel}){
         //AI and user
         this.player = player;
         this.dealer = new Player('Dealer');
         
-        this.playerCards = playerCards;
-        this.dealerCards = dealerCards;
+        this.tabel = tabel;
 
         this.deck = new Deck();
 
@@ -34,22 +34,22 @@ class Game{
             const playerCard = this.deck.pickOne();
 
             this.player.hand.addCard(playerCard);
-            this.playerCards.appendChild(playerCard.render());
+            this.tabel.showPlayerCard(playerCard.render());
 
             
             const dealerCard = this.deck.pickOne();
 
             this.dealer.hand.addCard(dealerCard);
-            this.dealerCards.appendChild(dealerCard.render());
+            this.tabel.showDealerCard(dealerCard.render());
 
         }
     }
 }
 
+const tabel = new Table(bindToHtml.bindById(playerCardsContainerId), bindToHtml.bindById(dealerCardsContainerId))
 const player = new Player('User');
 
 const game = new Game({
     player,
-    playerCards: bindToHtml.bindById(playerCardsContainerId),
-    dealerCards: bindToHtml.bindById(dealerCardsContainerId),
+    tabel
 });
---
+++ app/scripts/Table.js
@@ -0,0 +1,15 @@
+export class Table{
+    constructor(playerCards, dealerCards){
+        this.playerCards = playerCards;
+        this.dealerCards = dealerCards;
+    }
+
+    showPlayerCard(card){
+        this.playerCards.appendChild(card);
+    }
+
+    
+    showDealerCard(card){
+        this.dealerCards.appendChild(card);
+    }
+}
