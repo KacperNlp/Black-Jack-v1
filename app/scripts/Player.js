538c77fcbf37b79df4ece0c06864c4fc15d49c51
Kacper Nalepa <kacper.nlp@wp.pl>
2021-01-15 19:18:35 +0100

    Getting hand strength

--- app/scripts/Player.js
+++ app/scripts/Player.js
@@ -14,4 +14,32 @@ export class Player{
     set score(score){
         this.#score += score;
     }
+
+    cardsWithTheSameWeight(weight){
+        return this.hand.cards.filter( card => card.weight === weight).length
+    }
+
+    getHandStrength(){
+        if(this.cardsWithTheSameWeight('A').length === 2){
+            return 21;
+        }
+
+        const cardsStrength = this.hand.cards.map((card, id, array) =>{
+
+            if(['K', 'Q', 'J'].includes(card.weight)){
+                return 10;
+            }else if(card.weight === 'A' && array.length === 2){
+                return 11;
+            }else if(card.weight === 'A' && array.length > 2){
+                return 1;
+            }else{
+                return parseInt(card.weight);
+            }
+
+        })
+
+        return cardsStrength.reduce((sum, currentWieght)=>{
+            return sum + currentWieght;
+        });
+    }
 }
